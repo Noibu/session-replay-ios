60b865ba876171e21378c839eb0989d39c56a684
Fernando L. <fernando.leguia@noibu.com>
2026-03-20 11:49:31 -0400

    Release 0.1.2

--- Package.swift
+++ Package.swift
@@ -13,8 +13,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "NoibuSessionReplay",
-            url: "https://github.com/Noibu/session-replay-ios/releases/download/0.1.1/NoibuSessionReplay.xcframework.zip",
-            checksum: "ae2cd01197f2c478cea800fbdd060fcc4713b66d1130d8bc70ba1fa6e736ac6f"
+            url: "https://github.com/Noibu/session-replay-ios/releases/download/0.1.2/NoibuSessionReplay.xcframework.zip",
+            checksum: "72d97cccf6d10ad9c6d8d68b0ae043f72a7e29b6bee2422217eaa2ba3a9cfc01"
         )
     ]
 )
