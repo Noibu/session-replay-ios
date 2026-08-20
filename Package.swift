b2cacf20aa75dbc86a8a46a5f0d590c41ee83c5f
Juan Armario <juan.armariomunoz@noibu.com>
2026-08-20 10:33:33 -0400

    Release 1.0.0-rc.3

--- Package.swift
+++ Package.swift
@@ -13,13 +13,13 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "NoibuSessionReplay",
-            url: "https://github.com/Noibu/session-replay-ios/releases/download/1.0.0-rc.2/NoibuSessionReplay.xcframework.zip",
-            checksum: "1b5fdea1558440712411d8f391a4cb0c71e169a52eec18cb3d090a490703d7bf"
+            url: "https://github.com/Noibu/session-replay-ios/releases/download/1.0.0-rc.3/NoibuSessionReplay.xcframework.zip",
+            checksum: "c8b968db75b380857ddc21606547dbe3209ab07af920ff3a06dbe147c7ba9497"
         ),
         .binaryTarget(
             name: "coreKit",
-            url: "https://github.com/Noibu/session-replay-ios/releases/download/1.0.0-rc.2/coreKit.xcframework.zip",
-            checksum: "723ea57e9285f5943efdeb87c2c63da6f2f5194729c52f1e8671a4d4c88ae8ec"
+            url: "https://github.com/Noibu/session-replay-ios/releases/download/1.0.0-rc.3/coreKit.xcframework.zip",
+            checksum: "eadef985b6aec4de934c9266c44ef9faa496bb31fcc373ab8c29e0d576c8e254"
         )
     ]
 )
